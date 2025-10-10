ffc8258a39825f9cd3844a0add6d919562e21f2b
coreykmx <coreyx@nycstudents.net>
2025-10-10 07:22:02 +0000

    a

--- VR_Unit2/VRLesson2_1Classwork/script.js
+++ VR_Unit2/VRLesson2_1Classwork/script.js
@@ -1,24 +1,44 @@
 window.addEventListener("DOMContentLoaded",function() {
   //Challenge 1: Create a variable scene for the <a-scene> component in the HTML
   
-  let scene = document.createElement("a-scene");
-  let cone = document.createElement( “a-cone” );
-
-
+  let scene = document.querySelector("a-scene");
 
   //Challenge 2: Create a variable for <a-dodecahedron> and create it.
 
-  let dodecahedron = document.createElement("a-scene");
+  let dodeca = document.createElement("a-dodecahedron");
+
   //Challenge 3: Change the <a-dodecahedron>'s position, radius and color
 
-
+  dodeca.setAttribute("position", {x:0, y:4, z:-2});
+  dodeca.setAttribute("color", "black");
+  dodeca.setAttribute("radius", 4);
   //Challenge 4: Add the <a-dodecahedron> to the scene
 
+  scene.append(dodeca);
 
   /* Challenge Bonus
   1) Use a for loop to create a lot of <a-dodecahedron> 
   2) Change each <a-dodecahedron>'s position to a random x, y, and z location
   3) Change each <a-dodecahedron>'s to a random color.  Hint: Use rgb( ) and string interpolation 
+  Math.floor(Math.random() * (max - min + 1)) + min;
   */
   
+  for (let i=0;i<100;i++){
+    let dodecahedron = document.createElement('a-dodecahedron');
+
+    let x = Math.floor(Math.random() * (41)) - 20;
+    let y = Math.floor(Math.random() * (41));
+    let z = Math.floor(Math.random() * (41)) - 20;
+
+    dodecahedron.setAttribute("position",{x:x,y:y,z:z});
+
+    let r = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+    let g = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+    let b = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+
+    dodecahedron.setAttribute("color", `rgb(${r},${g},${b})`);
+
+    scene.append(dodecahedron);
+
+  }
 })
