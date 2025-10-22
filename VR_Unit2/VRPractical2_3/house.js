907448905340c7de9648f971f4f201570aced3cd
coreykmx <coreyx@nycstudents.net>
2025-10-22 16:26:42 +0000

    a

--- VR_Unit2/VRPractical2_3/house.js
+++ VR_Unit2/VRPractical2_3/house.js
@@ -2,19 +2,19 @@ class House{
     constructor(x,y,z){
         this.obj = document.createElement("a-entity");
         let base = document.createElement("a-box");
-        base.setAttribute("scale",{x:4,y:4,z:4});
-        base.setAttribute("position",{x:0,y:2,z:0});
+        base.setAttribute("scale",{x:2,y:2,z:2});
+        base.setAttribute("position",{x:0,y:1,z:0});
         base.setAttribute("color", "grey");
         let roof = document.createElement("a-cone");
-        roof.setAttribute("radius-bottom", 3.5);
-        roof.setAttribute("height", 3);
+        roof.setAttribute("radius-bottom", 2);
+        roof.setAttribute("height", 1.5);
         roof.setAttribute("segments-radial",4);
         roof.setAttribute("rotation",{x:0,y:45,z:0});
-        roof.setAttribute("position",{x:0,y:5,z:0});
+        roof.setAttribute("position",{x:0,y:2.5,z:0});
         roof.setAttribute("color", "brown");
         let door = document.createElement("a-box");
-        door.setAttribute("scale",{x:1,y:2,z:0.1});
-        door.setAttribute("position",{x:0,y:1,z:2});
+        door.setAttribute("scale",{x:0.5,y:1,z:0.1});
+        door.setAttribute("position",{x:0,y:0.5,z:1});
         door.setAttribute("color", "orange");
 
         this.obj.append(base);
