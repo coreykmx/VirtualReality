d5f5fc22c129d562fcb091644325c304742ff461
coreykmx <coreyx@nycstudents.net>
2025-11-26 17:25:41 +0000

    a

--- VR_Unit3/VRLesson3_5aClasswork/script.js
+++ VR_Unit3/VRLesson3_5aClasswork/script.js
@@ -1,14 +1,15 @@
 let rnd = (l,u) => Math.random() * (u-l) + l
-let scene, camera, target, ball, cones = [];
+let scene, camera, target,target2, ball, cones = [];
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   camera = document.querySelector("a-camera");
   target = document.querySelector("#target");
   // Challenge 2 - Create a variable for the second target
+  target2 = document.querySelector("#target2");
   ball = document.querySelector("#ball");
   ball.x = -1;
-  ball.dx = -0.002;
+  ball.dx = -0.2;
   
   setTimeout(loop,100);
 })
@@ -19,13 +20,16 @@ function loop(){
   ball.object3D.position.x = ball.x
   let d1 = distance(ball, target);
   //Challenge 3 - Store the distance between the ball and the second target
-
+  let d2 = distance(ball, target2);
   output.setAttribute("value",`d1= ${d1}\nd2=${d2}`);
 
   /*  Challenge 4
       Modify the decision below so that the direction of the ball is reversed 
       when the distance between target 1 OR target 2 is less than 1
   */
+  if(d2 < 1){
+    ball.dx = -ball.dx;
+  }
   if(d1 < 1){
     ball.dx = -ball.dx;
   }
